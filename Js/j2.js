3592cea75d4ea38fda6bc857b891cad64c346451
Saaddox <122133324+Saaddox@users.noreply.github.com>
2023-01-07 10:44:41 +0100

    Add files via upload

---
+++ Js/j2.js
@@ -0,0 +1,8 @@
+function affiche(){
+    var nom = document.forme.name.value;
+    var prenom = document.forme.pname.value;
+    var tel = document.forme.tel.value;
+    var con = document.forme.contry.value;
+    var char =" Votre registration a était bien enregistré mr "+nom+" "+prenom;
+    alert(char);
+}
